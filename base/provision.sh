244d51ab879be18501bdeadeae9a5c8f233bdc45
Brett Benyo <brett.benyo@raytheon.com>
2020-01-17 18:00:30 -0500

    Save provisioner scripts in /opt/provisioners

--- base/provision.sh
+++ base/provision.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -x
-# Provision base
 
-# Install default packages
-date > /opt/provisioner-date
-echo "base" >> /opt/provisioner-packages
+# Bookkeeping
+mkdir -p /opt/provisioners/base
+rm /opt/provisioners/base/provision.sh
 
+## Base provisioner
 sudo apt-get -y install openjdk-8-jdk-headless
 sudo apt-get -y install python3
 sudo apt-get -y install python
+
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python-pip
+
+pip install --upgrade setuptools
+pip3 install --upgrade setuptools
 
 mkdir /opt/base
 cd /opt
@@ -20,3 +26,10 @@ cd /opt/base
 tar xzvf /opt/base/test.tar.gz
 
 ls -ltr /opt/base
+
+# Install default packages
+date > /opt/provisioner-date
+# Base only overwrites provisioner-packages
+echo "base" > /opt/provisioner-packages
+# Save this provisioner script
+cp /tmp/provision.sh /opt/provisioners/base
