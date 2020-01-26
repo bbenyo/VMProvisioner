f232282d32b656ccf86b129708d018b62b5e4a30
Brett Benyo <brett.benyo@raytheon.com>
2020-01-26 15:44:34 -0500

    Store output in dict

--- scapy/files/tools/arpmon.py
+++ scapy/files/tools/arpmon.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import sys
-from scapy.all import sniff
+from scapy.all import sniff, ARP
 
-whosHere = []
+whosHere = {}
 
 def arp_display(pkt):
     if pkt[ARP].op == 1: # who-has
         return f"Request {pkt[ARP].psrc} is asking about {pkt[ARP].pdst}"
-    elif pkt[ARP].op == 2:
-        whosHere[pkt[ARP].hwsrc] = pkt[ARP].psrc
+    if pkt[ARP].op == 2:
+        whosHere[str(pkt[ARP].hwsrc)] = pkt[ARP].psrc
         return f"*Response: {pkt[ARP].hwsrc} has address {pkt[ARP].psrc}"
+    return "Unknown op: {pkt[ARP].op}"
 
-packets = sniff(count=10)
+packets = sniff(prn=arp_display, filter="arp", store=0, count=100)
+
 print(packets.summary())
-
 print(str(whosHere))
